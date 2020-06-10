746ec903531be1fb540cfd4e1623ce33157fe058
Porter lyman <porter.lyman@gmail.com>
2020-06-10 10:55:30 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("test");
+/* var x=5;
+console.log(x)
+x=x+9;
+console.log(x)
+ */
+
+ var x=
+ console.log(x)
+
+ console.log(1 === 1);
+
+ function buttonClicked() {
+     console.log("You clicked me");
+     alert("you clicked me")
+     document.getElementById("buttonText").innerHTML="clicked"
+
+
+ }
+function buttonSubmission() {
+    var firstName = document.getElementById("fName").value;
+    console.log(firstName)
+    if (firstName ==="Chipman") {
+        alert("correct")
+    }
+    else{
+        alert("incorrect")
+    }
+}
